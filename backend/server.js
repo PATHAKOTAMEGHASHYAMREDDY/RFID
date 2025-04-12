a11f23cad40579c6f86b9ce9359a75e107ea2fc6
BHAVESH <kallurbhavesh@gmail.com>
2025-04-12 22:50:58 +0530

    full work done

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-app.use(cors());
+app.use(cors({
+  origin:"*",
+  methods:["GET","POST","PUT","DELETE"],
+  allowedHeaders:["Content-Type","Authorization"],
+  credentials:true,
+}));
 app.use(express.json());
 
 // API to handle RFID scans (for web simulation)
